b15caf600b1beb8e5f504c723ddd83809f943bc1
David J. Bosak <dbosak01@gmail.com>
2025-12-01 12:17:05 -0500

    Try jpg, png, and svg

--- tests/testthat/test-sgrender.R
+++ tests/testthat/test-sgrender.R
@@ -62,28 +62,28 @@ test_that("sgrender2: Render output types works.", {
   #
   # expect_equal(file.exists(res$path), TRUE)
   #
-  # # PNG
-  # fp <- file.path(base_path, "output/test.png")
-  #
-  # if (file.exists(fp)) {
-  #   file.remove(fp)
-  # }
-  #
-  # res <- sgrender(p, output_type = "png", file_path = fp)
-  #
-  # expect_equal(file.exists(res$path), TRUE)
-  #
-  # # SVG
-  # fp <- file.path(base_path, "output/test.svg")
-  #
-  # if (file.exists(fp)) {
-  #   file.remove(fp)
-  # }
-  #
-  # res <- sgrender(p, output_type = "svg", file_path = fp)
-  #
-  # expect_equal(file.exists(res$path), TRUE)
-  #
+  # PNG
+  fp <- file.path(base_path, "output/test.png")
+
+  if (file.exists(fp)) {
+    file.remove(fp)
+  }
+
+  res <- sgrender(p, output_type = "png", file_path = fp)
+
+  expect_equal(file.exists(res$path), TRUE)
+
+  # SVG
+  fp <- file.path(base_path, "output/test.svg")
+
+  if (file.exists(fp)) {
+    file.remove(fp)
+  }
+
+  res <- sgrender(p, output_type = "svg", file_path = fp)
+
+  expect_equal(file.exists(res$path), TRUE)
+
   # # TIF
   # fp <- file.path(base_path, "output/test.tif")
   #
